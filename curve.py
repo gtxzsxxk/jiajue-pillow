af2d4eb903678484f7c9c61823deb0d18b7b6958
THUIRT <thuirt@thuirt.com>
2023-07-14 21:34:37 +0800

    Impplemented all

--- curve.py
+++ curve.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-fp = open("dataset_test/splice_SPRING2001+0.txt", "r")
+fp = open("dataset_test/splice_SPRING2001+5.txt", "r")
 text = fp.read()
 fp.close()
 X = []
@@ -10,11 +10,12 @@ for i in text.split('\n'):
     if i == "":
         continue
     x, y = i.split(' ')
-    v_centerize=np.array([0,0.05])
-    v_0=np.array([(float(x)),(float(y))])
-    theta=math.pi/2
-    R=np.array([[math.cos(theta),-math.sin(theta)],[math.sin(theta),math.cos(theta)]])
-    v_1=np.matmul(R,v_0)+v_centerize
+    v_centerize = np.array([0, 0.00])
+    v_0 = np.array([(float(x)), (float(y))])
+    theta = math.pi/2
+    R = np.array([[math.cos(theta), -math.sin(theta)],
+                 [math.sin(theta), math.cos(theta)]])
+    v_1 = np.matmul(R, v_0)+v_centerize
     X.append(v_1[0])
     Y.append(v_1[1])
 axises = plt.gca()
@@ -26,56 +27,61 @@ axises.set_aspect(1)
 plt.xlim(-0.12, 0.12)
 plt.ylim(-0.07, 0.18)
 plt.scatter(X, Y)
-first_node=[]
-first_node_index=0
-def PickFirstNode():
-    global first_node,first_node_index
-    max_dist=999
-    for i in range(0,X.__len__()):
-        if Y[i]>=0:
-            continue
-        if abs(X[i])<max_dist:
-            max_dist=abs(X[i])
-            first_node=np.array([X[i],Y[i]])
-            first_node_index=i
+first_node = []
+first_node_index = 0
 
-node_sorted=[] #vectors
-            
+
+def PickFirstNode():
+    global first_node, first_node_index
+    max_dist = 999
+    for i in range(0, X.__len__()):
+        if Y[i] >= 0:
+            continue
+        if abs(X[i]) < max_dist:
+            max_dist = abs(X[i])
+            first_node = np.array([X[i], Y[i]])
+            first_node_index = i
+
+
+node_sorted = []  # vectors
+
+
 def CcwSort():
     global node_sorted
-    last_node=first_node
-    visited=[]
-    for i in range(0,X.__len__()):
+    last_node = first_node
+    visited = []
+    for i in range(0, X.__len__()):
         visited.append(False)
-    visited[first_node_index]=True
-    visited_cnt=1
-    while visited_cnt<X.__len__():
-        max_dist=999
-        nearest_node=[]
-        nearest_node_index=0
+    visited[first_node_index] = True
+    visited_cnt = 1
+    while visited_cnt < X.__len__():
+        max_dist = 999
+        nearest_node = []
+        nearest_node_index = 0
         node_sorted.append(last_node)
-        for i in range(0,X.__len__()):
+        for i in range(0, X.__len__()):
             if visited[i] is True:
                 continue
-            dx=X[i]-last_node[0]
-            dy=Y[i]-last_node[1]
-            dist=math.sqrt(dx**2+dy**2)
-            if dist<max_dist:
-                max_dist=dist
-                if last_node[0]==first_node[0] and last_node[1]==first_node[1] and dx<0:
+            dx = X[i]-last_node[0]
+            dy = Y[i]-last_node[1]
+            dist = math.sqrt(dx**2+dy**2)
+            if dist < max_dist:
+                max_dist = dist
+                if last_node[0] == first_node[0] and last_node[1] == first_node[1] and dx < 0:
                     continue
-                nearest_node=[X[i],Y[i]]
-                nearest_node_index=i
-        visited[nearest_node_index]=True
-        visited_cnt+=1
-        last_node=nearest_node
+                nearest_node = [X[i], Y[i]]
+                nearest_node_index = i
+        visited[nearest_node_index] = True
+        visited_cnt += 1
+        last_node = nearest_node
     node_sorted.append(last_node)
-    
+
+
 PickFirstNode()
 CcwSort()
-plt.scatter([first_node[0]],[first_node[1]],marker='v')
+plt.scatter([first_node[0]], [first_node[1]], marker='v')
 for i in node_sorted:
-    plt.ion()  #打开交互模式
+    plt.ion()  # 打开交互模式
     axises = plt.gca()
     axises.spines["right"].set_color('none')
     axises.spines["top"].set_color('none')
@@ -84,55 +90,62 @@ for i in node_sorted:
     axises.set_aspect(1)
     plt.xlim(-0.12, 0.12)
     plt.ylim(-0.07, 0.18)
-    plt.scatter([i[0]],[i[1]],marker='+')
+    plt.scatter([i[0]], [i[1]], marker='+')
     plt.show()
     plt.pause(0.001)
     # plt.clf()  #清除图像
 
-def move_nodes(src,b):
-    res=[]
+
+def move_nodes(src, b):
+    res = []
     for i in src:
         res.append(np.array(i)+b)
     return res
 
-def rotate_cw_nodes(src,angle):
-    res=[]
-    R=np.array([[math.cos(-angle),-math.sin(-angle)],[math.sin(-angle),math.cos(-angle)]])
+
+def rotate_cw_nodes(src, angle):
+    res = []
+    R = np.array([[math.cos(-angle), -math.sin(-angle)],
+                 [math.sin(-angle), math.cos(-angle)]])
     for i in src:
-        res.append(np.matmul(R,i))
+        res.append(np.matmul(R, i))
     return res
-        
-curve=[]
+
+
+curve = []
+
+
 def generate_curve(dir):
     global curve
-    temp_nodes=node_sorted
-    start_index=1
-    if dir=='ccw':
-        start_index=node_sorted.__len__()-1
-    angle_accumulated=0
-    while angle_accumulated<math.pi/2:
-        tangent=abs(temp_nodes[start_index][0]/temp_nodes[start_index][1])
-        d_theta=math.atan(tangent)
-        radius=math.sqrt(temp_nodes[start_index][0]**2+temp_nodes[start_index][1]**2)
-        s=radius*d_theta
-        if dir=='cw':
-            curve=move_nodes(curve,np.array([-s,0]))
+    temp_nodes = node_sorted
+    start_index = 1
+    if dir == 'ccw':
+        start_index = node_sorted.__len__()-1
+    angle_accumulated = 0
+    while angle_accumulated < math.pi/2:
+        tangent = abs(temp_nodes[start_index][0]/temp_nodes[start_index][1])
+        d_theta = math.atan(tangent)
+        radius = math.sqrt(temp_nodes[start_index]
+                           [0]**2+temp_nodes[start_index][1]**2)
+        s = radius*d_theta
+        if dir == 'cw':
+            curve = move_nodes(curve, np.array([-s, 0]))
         else:
-            curve=move_nodes(curve,np.array([s,0]))
-        curve.append(np.array([0,-radius]))
-        if dir=='cw':
-            temp_nodes=rotate_cw_nodes(temp_nodes,d_theta)
+            curve = move_nodes(curve, np.array([s, 0]))
+        curve.append(np.array([0, -radius]))
+        if dir == 'cw':
+            temp_nodes = rotate_cw_nodes(temp_nodes, d_theta)
         else:
-            temp_nodes=rotate_cw_nodes(temp_nodes,-d_theta)
-        angle_accumulated+=d_theta
-        if dir=='cw':
-            start_index+=1
+            temp_nodes = rotate_cw_nodes(temp_nodes, -d_theta)
+        angle_accumulated += d_theta
+        if dir == 'cw':
+            start_index += 1
         else:
-            start_index-=1
-        
+            start_index -= 1
+
         # 演示功能
-        plt.ion()  #打开交互模式
-        plt.clf()  #清除图像
+        plt.ion()  # 打开交互模式
+        plt.clf()  # 清除图像
         axises = plt.gca()
         axises.spines["right"].set_color('none')
         axises.spines["top"].set_color('none')
@@ -143,44 +156,46 @@ def generate_curve(dir):
         plt.ylim(-0.2, 0.2)
         # plt.scatter([i[0]],[i[1]],marker='+')
         for k in curve:
-           plt.scatter([k[0]],[k[1]],marker='+')
+            plt.scatter([k[0]], [k[1]], marker='+')
         for k in temp_nodes:
-            plt.scatter([k[0]],[k[1]],marker='.')
+            plt.scatter([k[0]], [k[1]], marker='.')
         plt.show()
         plt.pause(0.01)
-    last_node=None
-    tap_len=20
-    tap_mode=False
-    tap_dist=2/100
-    tap_len_const=20
-    while True and tap_len>0:
+    last_node = None
+    tap_len = 20
+    tap_mode = False
+    tap_dist = 2/100
+    tap_len_const = 20
+    while True and tap_len > 0:
         if tap_mode:
-            curve.append(np.array([temp_nodes[start_index][0],temp_nodes[start_index][1]-\
+            curve.append(np.array([temp_nodes[start_index][0], temp_nodes[start_index][1] -
                                    tap_dist*(tap_len_const-tap_len)/tap_len_const]))
-            plt.scatter([temp_nodes[start_index][0]],[temp_nodes[start_index][1]-\
-                tap_dist*(tap_len_const-tap_len)/tap_len_const],marker='+')
-            tap_len-=1
+            plt.scatter([temp_nodes[start_index][0]], [temp_nodes[start_index][1] -
+                                                       tap_dist*(tap_len_const-tap_len)/tap_len_const], marker='+')
+            tap_len -= 1
         else:
             if last_node is not None:
-                dy=temp_nodes[start_index][1]-last_node[1]
-                if dir=='cw':
-                    if dy>0:
-                        tap_mode=True
+                dy = temp_nodes[start_index][1]-last_node[1]
+                if dir == 'cw':
+                    if dy > 0:
+                        tap_mode = True
                 else:
-                    if dy>0:
-                        tap_mode=True
-            curve.append(np.array([temp_nodes[start_index][0],temp_nodes[start_index][1]]))
-            plt.scatter([temp_nodes[start_index][0]],[temp_nodes[start_index][1]],marker='+')
-        last_node=temp_nodes[start_index]
-        if dir=='cw':
-            start_index+=1
+                    if dy > 0:
+                        tap_mode = True
+            curve.append(
+                np.array([temp_nodes[start_index][0], temp_nodes[start_index][1]]))
+            plt.scatter([temp_nodes[start_index][0]], [
+                        temp_nodes[start_index][1]], marker='+')
+        last_node = temp_nodes[start_index]
+        if dir == 'cw':
+            start_index += 1
         else:
-            start_index-=1
+            start_index -= 1
         plt.show()
         plt.pause(0.01)
 
+
 generate_curve('cw')
-curve=move_nodes(curve,np.array([-curve[0][0],0]))
+curve = move_nodes(curve, np.array([-curve[0][0], 0]))
 generate_curve('ccw')
 plt.pause(120)
-    
--- surface.py
+++ surface.py
@@ -2,142 +2,163 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+
 def PickFirstNode(nodes):
-    max_dist=999
-    first_node=None
-    first_node_index=0
-    for i in range(0,nodes.__len__()):
-        if nodes[i][1]>=0:
+    max_dist = 999
+    first_node = None
+    first_node_index = 0
+    for i in range(0, nodes.__len__()):
+        if nodes[i][1] >= 0:
             continue
-        if abs(nodes[i][0])<max_dist:
-            max_dist=abs(nodes[i][0])
-            first_node=np.array([nodes[i][0],nodes[i][1]])
-            first_node_index=i
-    return [first_node,first_node_index]
-            
-def CcwSort(node2sort,first_node,first_node_index):
-    sorted_node=[]
-    last_node=first_node
-    visited=[]
-    for i in range(0,node2sort.__len__()):
+        if abs(nodes[i][0]) < max_dist:
+            max_dist = abs(nodes[i][0])
+            first_node = np.array([nodes[i][0], nodes[i][1]])
+            first_node_index = i
+    return [first_node, first_node_index]
+
+
+def CcwSort(node2sort, first_node, first_node_index):
+    sorted_node = []
+    last_node = first_node
+    visited = []
+    for i in range(0, node2sort.__len__()):
         visited.append(False)
-    visited[first_node_index]=True
-    visited_cnt=1
-    while visited_cnt<node2sort.__len__():
-        max_dist=999
-        nearest_node=[]
-        nearest_node_index=0
+    visited[first_node_index] = True
+    visited_cnt = 1
+    while visited_cnt < node2sort.__len__():
+        max_dist = 999
+        nearest_node = []
+        nearest_node_index = 0
         sorted_node.append(last_node)
-        for i in range(0,node2sort.__len__()):
+        for i in range(0, node2sort.__len__()):
             if visited[i] is True:
                 continue
-            dx=node2sort[i][0]-last_node[0]
-            dy=node2sort[i][1]-last_node[1]
-            dist=math.sqrt(dx**2+dy**2)
-            if dist<max_dist:
-                max_dist=dist
-                if last_node[0]==first_node[0] and last_node[1]==first_node[1] and dx<0:
+            dx = node2sort[i][0]-last_node[0]
+            dy = node2sort[i][1]-last_node[1]
+            dist = math.sqrt(dx**2+dy**2)
+            if dist < max_dist:
+                max_dist = dist
+                if last_node[0] == first_node[0] and last_node[1] == first_node[1] and dx < 0:
                     continue
-                nearest_node=node2sort[i]
-                nearest_node_index=i
-        visited[nearest_node_index]=True
-        visited_cnt+=1
-        last_node=nearest_node
+                nearest_node = node2sort[i]
+                nearest_node_index = i
+        visited[nearest_node_index] = True
+        visited_cnt += 1
+        last_node = nearest_node
     sorted_node.append(last_node)
     return sorted_node
 
-def move_nodes(src,b):
-    res=[]
+
+def move_nodes(src, b):
+    res = []
     for i in src:
         res.append(np.array(i)+b)
     return res
 
-def rotate_cw_nodes(src,angle):
-    res=[]
-    R=np.array([[math.cos(-angle),-math.sin(-angle)],[math.sin(-angle),math.cos(-angle)]])
+
+def rotate_cw_nodes(src, angle):
+    res = []
+    R = np.array([[math.cos(-angle), -math.sin(-angle)],
+                 [math.sin(-angle), math.cos(-angle)]])
     for i in src:
-        res.append(np.matmul(R,i))
+        res.append(np.matmul(R, i))
     return res
 
-def generate_curve_from_sample(sample_nodes,is_ccw,tap_len=20,tap_dist=2/100,tap_len_const=20):
-    curve_generated=[]
-    temp_nodes=sample_nodes
-    start_index=1
+
+def generate_curve_from_sample(sample_nodes, is_ccw, tap_len=20, tap_dist=2/100, tap_len_const=20):
+    curve_generated = []
+    temp_nodes = sample_nodes
+    start_index = 1
     if is_ccw:
-        start_index=sample_nodes.__len__()-1
-    angle_accumulated=0
-    while angle_accumulated<math.pi/2:
-        tangent=abs(temp_nodes[start_index][0]/temp_nodes[start_index][1])
-        d_theta=math.atan(tangent)
-        radius=math.sqrt(temp_nodes[start_index][0]**2+temp_nodes[start_index][1]**2)
-        s=radius*d_theta
-        if is_ccw is False:
-            curve_generated=move_nodes(curve_generated,np.array([-s,0]))
+        start_index = sample_nodes.__len__()-1
+    angle_accumulated = 0
+    while angle_accumulated < math.pi/2:
+        tangent = abs(temp_nodes[start_index][0]/temp_nodes[start_index][1])
+        d_theta = math.atan(tangent)
+        radius = math.sqrt(temp_nodes[start_index]
+                           [0]**2+temp_nodes[start_index][1]**2)
+        s = radius*d_theta
+        if not is_ccw:
+            curve_generated = move_nodes(curve_generated, np.array([-s, 0]))
         else:
-            curve_generated=move_nodes(curve_generated,np.array([s,0]))
-        curve_generated.append(np.array([0,-radius]))
-        if is_ccw is False:
-            temp_nodes=rotate_cw_nodes(temp_nodes,d_theta)
+            curve_generated = move_nodes(curve_generated, np.array([s, 0]))
+        curve_generated.append(np.array([0, -radius]))
+        if not is_ccw:
+            temp_nodes = rotate_cw_nodes(temp_nodes, d_theta)
         else:
-            temp_nodes=rotate_cw_nodes(temp_nodes,-d_theta)
-        angle_accumulated+=d_theta
+            temp_nodes = rotate_cw_nodes(temp_nodes, -d_theta)
+        angle_accumulated += d_theta
         if is_ccw is False:
-            start_index+=1
+            start_index += 1
         else:
-            start_index-=1
-    last_node=None
-    tap_mode=False
-    while True and tap_len>0:
+            start_index -= 1
+    last_node = None
+    tap_mode = False
+    while True and tap_len > 0:
         if tap_mode:
-            curve_generated.append(np.array([temp_nodes[start_index][0],temp_nodes[start_index][1]-\
-                                tap_dist*(tap_len_const-tap_len)/tap_len_const]))
-            tap_len-=1
+            curve_generated.append(np.array([temp_nodes[start_index][0], temp_nodes[start_index][1] -
+                                             tap_dist*(tap_len_const-tap_len)/tap_len_const]))
+            tap_len -= 1
         else:
             if last_node is not None:
-                dy=temp_nodes[start_index][1]-last_node[1]
+                dy = temp_nodes[start_index][1]-last_node[1]
                 if is_ccw is False:
-                    if dy>0:
-                        tap_mode=True
+                    if dy > 0:
+                        tap_mode = True
                 else:
-                    if dy>0:
-                        tap_mode=True
-            curve_generated.append(np.array([temp_nodes[start_index][0],temp_nodes[start_index][1]]))
-        last_node=temp_nodes[start_index]
+                    if dy > 0:
+                        tap_mode = True
+            curve_generated.append(
+                np.array([temp_nodes[start_index][0], temp_nodes[start_index][1]]))
+        last_node = temp_nodes[start_index]
         if is_ccw is False:
-            start_index+=1
+            start_index += 1
         else:
-            start_index-=1
+            start_index -= 1
     return curve_generated
-    
 
-def generate_curve(filename,v_centerize=np.array([0,0]),theta=math.pi/2):
+
+def generate_curve(filename, v_centerize=np.array([0, 0.05]), theta=math.pi/2):
     fp = open(filename, "r")
     text = fp.read()
     fp.close()
-    all_nodes=[]
+    all_nodes = []
     for i in text.split('\n'):
         if i == "":
             continue
         x, y = i.split(' ')
-        v_0=np.array([(float(x)),(float(y))])
-        R=np.array([[math.cos(theta),-math.sin(theta)],[math.sin(theta),math.cos(theta)]])
-        v_1=np.matmul(R,v_0)+v_centerize
+        v_0 = np.array([(float(x)), (float(y))])
+        R = np.array([[math.cos(theta), -math.sin(theta)],
+                     [math.sin(theta), math.cos(theta)]])
+        v_1 = np.matmul(R, v_0)+v_centerize
         all_nodes.append(v_1)
-    first_node=[]
-    first_node_index=0
-    [first_node,first_node_index]=PickFirstNode(all_nodes)
-    node_sorted=CcwSort(all_nodes,first_node,first_node_index)
-    curve=generate_curve_from_sample(node_sorted,False)
-    curve=move_nodes(curve,np.array([-curve[0][0],0]))
-    curve+=generate_curve_from_sample(node_sorted,True)
-    X=[]
-    Y=[]
+    first_node = []
+    first_node_index = 0
+    [first_node, first_node_index] = PickFirstNode(all_nodes)
+    node_sorted = CcwSort(all_nodes, first_node, first_node_index)
+    curve_a = generate_curve_from_sample(node_sorted, False)
+    curve_a = move_nodes(curve_a, np.array([-curve_a[0][0], 0]))
+    curve_b = generate_curve_from_sample(node_sorted, True)
+    curve_b = move_nodes(curve_b, np.array([-curve_b[0][0], 0]))
+    curve = curve_a+curve_b
+    X = []
+    Y = []
     for i in curve:
         X.append(i[0])
         Y.append(i[1])
-    plt.gca().set_aspect(1)
-    plt.scatter(X,Y,marker='+')
+
+    plt.clf()  # 清除图像
+    axises = plt.gca()
+    axises.spines["right"].set_color('none')
+    axises.spines["top"].set_color('none')
+    axises.spines['left'].set_position(('data', 0))
+    axises.spines['bottom'].set_position(('data', 0))
+    axises.set_aspect(1)
+    plt.xlim(-0.3, 0.3)
+    plt.ylim(-0.2, 0.2)
+    plt.scatter(X, Y, marker='+')
     plt.show()
     return curve
-    
-generate_curve("dataset_test/splice_SPRING2001+0.txt")
+
+
+generate_curve("dataset_test/splice_SPRING2001+0.txt",v_centerize=np.array([0, 0.02]))
